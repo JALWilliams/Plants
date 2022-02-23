14ead60145b8ef177db73c39e549e947aa27d75b
JALWilliams <just.williams04@gmail.com>
2022-02-23 17:21:55 +0100

    filter working, but need to have fewer filters

--- Controllers/filter.js
+++ Controllers/filter.js
@@ -7,14 +7,33 @@ var FilterController = {
   },
 
   Filter: function(req,res){
-  
-    Plant.find({ Humidity: req.query.humidity}).exec(function (err, filtered_plants){
+    // console.log(req.query)
+    if(req.query.sunlight=='' && req.query.moisture=='' && req.query.indoorFlowering==''){
+      res.redirect('/plants');
+    } else{   
+    
+    Plant.find({ 
+      Sunlight: req.query.sunlight,
+      Moisture: req.query.moisture,
+      Indoor_Flowering: req.query.indoorFlowering,
+      Toxic_Dogs: req.query.toxicToDogs,
+      Toxic_Cats: req.query.toxicToCats,
+      Indoor_Height: req.query.indoorHeight,
+      Indoor_Spread: req.query.indoorSpread
+      }).exec(function (err, filtered_plants){
       if (err){ throw err;}
-      // console.log(filtered_plants)
-
       res.render('filteredPlants', {filteredPlants : filtered_plants});
-    });
+    });}
   }
 }
 
 module.exports = FilterController;
+
+// {
+//   sunlight: 'full-sun-to-part-sun',
+//   moisture: 'low',
+//   'indoor-flowering': 'yes',
+//   'toxic-to-dogs': 'yes',
+//   'toxic-to-cats': 'yes',
+//   'indoor-spread': [ '0.25-to-0.75-feet', '0.00-to-0.00-feet' ]
+// }
