210fc7079a1cd92628eeafd1c97e9fa4f2a004b5
JALWilliams <just.williams04@gmail.com>
2022-02-23 14:45:10 +0100

    filtering methodology understoor, waiting on final filter

--- Controllers/filter.js
+++ Controllers/filter.js
@@ -1,7 +1,19 @@
+const Plant = require('../models/plants')
+
 var FilterController = {
   
   New: function(req, res){
     res.render('form/index');
+  },
+
+  Filter: function(req,res){
+  
+    Plant.find({ Humidity: req.query.humidity}).exec(function (err, filtered_plants){
+      if (err){ throw err;}
+      // console.log(filtered_plants)
+
+      res.render('filteredPlants', {filteredPlants : filtered_plants});
+    });
   }
 }
 
--- Routes/filter.js
+++ Routes/filter.js
@@ -4,5 +4,6 @@ var router = express.Router();
 var FilterController = require('../controllers/filter')
 
 router.get('/new', FilterController.New)
+router.get('/results', FilterController.Filter)
 
 module.exports = router;
