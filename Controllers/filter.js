8a5a476751ab50eb63b15bd09f9f6d00121820eb
JALWilliams <just.williams04@gmail.com>
2022-02-24 12:25:12 +0100

    Filter working and plant doc integrated

--- Controllers/filter.js
+++ Controllers/filter.js
@@ -8,33 +8,18 @@ var FilterController = {
 
   Filter: function(req,res){
     // console.log(req.query)
-    if(req.query.sunlight=='' && req.query.moisture=='' && req.query.indoorFlowering=='' && req.query.toxicToAnimals==''){
-      res.redirect('/plants');
-    } else{   
-    
     Plant.find({ 
       Sunlight: req.query.sunlight,
       Moisture: req.query.moisture,
       Indoor_Flowering: req.query.indoorFlowering,
-      Toxic_Dogs: req.query.toxicToAnimals, 
-      // Toxic_Dogs: req.query.toxicToDogs,
-      // Toxic_Cats: req.query.toxicToCats,
-      Indoor_Height: req.query.indoorHeight,
-      Indoor_Spread: req.query.indoorSpread
+      Toxic_Dogs: req.query.toxicToAnimals  
+      // Indoor_Height: req.query.indoorHeight,
+      // Indoor_Spread: req.query.indoorSpread
       }).exec(function (err, filtered_plants){
       if (err){ throw err;}
       res.render('filteredPlants', {filteredPlants : filtered_plants});
     });}
-  }
+  
 }
 
 module.exports = FilterController;
-
-// {
-//   sunlight: 'full-sun-to-part-sun',
-//   moisture: 'low',
-//   'indoor-flowering': 'yes',
-//   'toxic-to-dogs': 'yes',
-//   'toxic-to-cats': 'yes',
-//   'indoor-spread': [ '0.25-to-0.75-feet', '0.00-to-0.00-feet' ]
-// }
