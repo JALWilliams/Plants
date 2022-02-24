1dba2137c995656e465c3e27cd08b11f0a3068ac
Justine Williams <just.williams04@gmail.com>
2022-02-24 13:56:44 +0100

    hopefully will be able to commit

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', function(req,res) {
   res.render('index')
 })
 
-module.exports = router;
+router.get('/filter/new', function(req, res) {
+  res.render('../views/form/index')
+})
+
+module.exports = router
